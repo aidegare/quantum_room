cc9984959e07b830119bd475f04d3f16f91f1380
aidegare <edgar.roussel@gmail.com>
2025-08-29 15:59:06 +0200

    cleanup, minor fixes and more access to files

--- TODO.md
+++ TODO.md
@@ -1,6 +1,17 @@
 - [x] generate the scene
 - [x] import the model
 - [x] add camera, lighting, helpers, onresize function
-- [ ] tweak camera, clean up code
+- [x] tweak camera, clean up code
 - [ ] test baking procedural material
+
+procedural materials:
+visualize everything in solid view using "Texture" option to see grid texture created earlier as image texture
+- [ ] create new UV map for baking -> **higlight it when creating all the seams**
+    - [ ] UV unwrap with min. seams possible -> think of everything as a cube, cylinder/ring
+    - [ ] turn on UV stretch in overlays to see and minimize stretch (distortion/stretch is fine for uniform colors)
+    - [ ] try smart UV project 
+objects having similar geometries can copy UV maps from the well unwrapped one (e.g. subdivided geometry)  
+- [ ] create image texture (4K, full white, no alpha if nothing transparent, 32-bit float) and save as Radiance HDR
+- [ ] ...
+
 - [ ] add other lights to get similar lighting as blender
--- src/main.js
+++ src/main.js
@@ -5,24 +5,26 @@ import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { DRACOLoader } from 'three/addons/loaders/DRACOLoader.js';
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 
+// SCENE SETUP
 const scene = new THREE.Scene();
-
-// Uncomment to see axes helper
-const axesHelper = new THREE.AxesHelper( 100 );
-axesHelper.position.set(50, 50, 50);
-scene.add( axesHelper );
-
-const canvasStats = document.getElementById('stats');
-const canvas = document.getElementById('experience');
-
-const stats = new Stats();
-canvasStats.appendChild( stats.dom );
-
-
 const sizes = {
   width: window.innerWidth,
   height: window.innerHeight,
 }
+
+// Uncomment to see axes helper
+const axesHelper = new THREE.AxesHelper( 100 );
+axesHelper.position.set(10, 10, 10);
+scene.add( axesHelper );
+
+
+
+const canvasStats = document.getElementById('stats');
+const canvas = document.getElementById('experience');
+const stats = new Stats();
+canvasStats.appendChild( stats.dom );
+
+
 
 const renderer = new THREE.WebGLRenderer({ canvas: canvas, antialias: true });
 renderer.setSize( sizes.width, sizes.height );
@@ -30,17 +32,18 @@ renderer.setPixelRatio( Math.min(window.devicePixelRatio, 2) );
 renderer.shadowMap.enabled = true;
 renderer.shadowMap.type = THREE.PCFSoftShadowMap; 
 renderer.toneMapping = THREE.ACESFilmicToneMapping;
-renderer.toneMappingExposure = 2;
-document.body.appendChild( renderer.domElement ); 
+renderer.toneMappingExposure = 1.5;
 
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+
+const camera = new THREE.PerspectiveCamera( 75, sizes.width / sizes.height, 0.1, 1000 );
 camera.position.set( 8, 8, -8);
-camera.lookAt( 0, 20, 0 );
-const cameraHelper = new THREE.CameraHelper(camera);
-scene.add(cameraHelper);
 
 const controls = new OrbitControls( camera, renderer.domElement );
 controls.update();
+
+
+
 
 const loader = new GLTFLoader();
 const dracoLoader = new DRACOLoader();
@@ -56,6 +59,8 @@ loader.load('./quantum_room.glb', function ( gltf ) {
 );
 scene.add( loader );
 
+
+
 const sun = new THREE.DirectionalLight( 0xffffff, 1 );
 sun.position.set( 20, 20, -10 );
 sun.castShadow = true;
@@ -68,35 +73,40 @@ sun.shadow.camera.bottom = -80;
 sun.shadow.normalBias = 0.5;
 scene.add( sun.target );
 scene.add( sun );
-// uncomment to see area of shadows cast and where light is
-const shadowHelper = new THREE.CameraHelper( sun.shadow.camera );
-scene.add( shadowHelper );
-const lightHelper = new THREE.DirectionalLightHelper( sun, 5 );
-scene.add( lightHelper );
 
+// uncomment to see area of shadows cast and where light is
+// const shadowHelper = new THREE.CameraHelper( sun.shadow.camera );
+// scene.add( shadowHelper );
+// const lightHelper = new THREE.DirectionalLightHelper( sun, 5 );
+// scene.add( lightHelper );
+
+
+
+
+
+// FUNCTIONS
 
 // handles the resizing of the window
 function onResize() {
   sizes.width = window.innerWidth;
   sizes.height = window.innerHeight;
-
   const aspect = sizes.width / sizes.height;
-  camera.left = -aspect * 50;
-  camera.right = aspect * 50;
-  camera.top = 50;
-  camera.bottom = -50;
+  camera.aspect = aspect;
   camera.updateProjectionMatrix();
-
   renderer.setSize(sizes.width, sizes.height);
   renderer.setPixelRatio( Math.min(window.devicePixelRatio, 2) );
 }
 
 
+window.addEventListener('resize', onResize); 
+
 
 
 function animate() {
+  requestAnimationFrame( animate );
   stats.update();
   onResize();
+  camera.lookAt( 0, 3, 0 );
   renderer.render( scene, camera );
 }
-renderer.setAnimationLoop( animate );
+animate();
